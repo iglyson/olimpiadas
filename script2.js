573b53f70c794bde63ea8aadf3e9cf9d6884b4ef
Iglyson <iglyson.andrey@escolar.ifrn.edu.br>
2023-01-26 11:36:50 -0300

    novo

--- script2.js
+++ script2.js
@@ -1,5 +1,17 @@
 /*let entrada = parseInt*/
 
 let section=document.querySelector("main")
-section.classList.add("dark")
-section.classList.remove("light")
+
+function todarkside(){
+    
+    section.classList.add("dark")
+    section.classList.remove("light")
+}
+
+function todarkside(){
+    
+    section.classList.add("light")
+    section.classList.remove("dark")
+}
+
+todarkside()
